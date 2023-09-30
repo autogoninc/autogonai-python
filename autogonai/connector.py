83db8d5eca33712a3e3887b199784c229bcf0f66
David Mbatuegwu <davidmbatuegwu@gmail.com>
2023-09-30 21:37:24 +0100

    Finished base library

--- autogonai/connector.py
+++ autogonai/connector.py
@@ -4,9 +4,6 @@ import requests
 from .errors import AutogonRequestError, AutogonServerError
 
 # Define a test header with an authorization token
-test_header = {
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzM2NTAyNzYzLCJqdGkiOiIwZTI4NDI4YWVhY2U0Mzg3YjRlMDY3ZjRkZDQ1MDU5YyIsInVzZXJfaWQiOjl9.mWhfLm8ptiYngvTpKYnp4ytQU08HqBh3Vux7_9gXZvc"
-}
 
 
 # Define a class named 'API'
@@ -37,6 +34,7 @@ class API:
         else:
             self.url = base_url
         self.api_key = api_key
+        self.header = {"X-AUG-KEY": api_key}
 
     # Define a method to send HTTP requests
     def send_request(self, endpoint, json_data=None, form_data=None, method="post"):
@@ -56,20 +54,20 @@ class API:
         # Send an HTTP request based on the specified method
         if method.lower() == "post":
             response = requests.post(
-                url, headers=test_header, json=json_data, data=form_data
+                url, headers=self.header, json=json_data, data=form_data
             )
         elif method.lower() == "get":
-            response = requests.get(url, headers=test_header)
+            response = requests.get(url, headers=self.header)
         elif method.lower() == "patch":
             response = requests.patch(
-                url, headers=test_header, json=json_data, data=form_data
+                url, headers=self.header, json=json_data, data=form_data
             )
         elif method.lower() == "put":
             response = requests.put(
-                url, headers=test_header, json=json_data, data=form_data
+                url, headers=self.header, json=json_data, data=form_data
             )
         elif method.lower() == "delete":
-            response = requests.delete(url, headers=test_header)
+            response = requests.delete(url, headers=self.header)
 
         # Handle exceptions that may occur during the request
 
