64f35d43679b3a5bbbb79a683563812dc50a32c8
VHEED <42419305+The-Vheed@users.noreply.github.com>
2023-10-05 22:17:01 +0100

    Update Documentation (#8)

--- examples/qore_test.py
+++ examples/qore_test.py
@@ -13,8 +13,8 @@ print("API KEY:", API_KEY)
 
 client = Client(api_key=API_KEY)
 
-response = client.Qore.VisionAI.stable_diffusion(
-    "A dad and his son walking down the street towards a park",
+response = client.Qore.VisionAI.image_generation(
+    "A dad and his son walking down the street towards a park", "512x512"
 )
 
 image = Image.open(client.Qore.VisionAI._process_image_to_bytes(response["image"]))
