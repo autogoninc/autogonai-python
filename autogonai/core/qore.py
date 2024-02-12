8d1ab0f4ed8e6011c88724eed3518ae4d1fe176e
Maxiron <naarons800@gmail.com>
2024-02-12 15:51:11 +0100

    Update Python version to 3.10

--- autogonai/core/qore.py
+++ autogonai/core/qore.py
@@ -18,16 +18,35 @@ class Vision:
 
     @staticmethod
     def _process_image_to_bytes(img_str: str):
-        """Processes images to BytesIO objects which can be loaded by the PIL library"""
+        """
+        Processes images to BytesIO objects which can be loaded by the PIL library
+        Args:
+            img_str (str): The image in base64 format.
+        Returns:
+            BytesIO: The image in bytes.
+        """
         return BytesIO(base64.b64decode(img_str.encode("utf-8")))
 
     @staticmethod
     def _download_image(img_url: str):
-        """Downloads image from a url and processes it to bytes"""
+        """
+        Downloads image from a url and processes it to bytes
+        Args:
+            img_url (str): The url of the image to be downloaded.
+        Returns:
+            BytesIO: The image in bytes.
+        """
         response = requests.get(img_url)
         return BytesIO(response.content)
 
     def text_detection(self, image) -> dict:
+        """
+        Detects text in an image.
+        Args:
+            image (str): The image to be processed.
+        Returns:
+            dict: The response from the API.
+        """
         body = {"image": image, "operation": "text_detection"}
         response = self.client.send_request(
             self.endpoint + "vision-ai/", form_data=body, method="post"
@@ -42,6 +61,13 @@ class Vision:
         return response
 
     def label_detection(self, image) -> dict:
+        """
+        Detects labels in an image.
+        Args:
+            image (str): The image to be processed.
+        Returns:
+            dict: The response from the API.
+        """
         body = {"image": image, "operation": "label_detection"}
         response = self.client.send_request(
             self.endpoint + "vision-ai/", form_data=body, method="post"
@@ -49,6 +75,13 @@ class Vision:
         return response
 
     def landmark_detection(self, image) -> dict:
+        """
+        Detects landmarks in an image.
+        Args:
+            image (str): The image to be processed.
+        Returns:
+            dict: The response from the API.
+        """
         body = {"image": image, "operation": "landmark_detection"}
         response = self.client.send_request(
             self.endpoint + "vision-ai/", form_data=body, method="post"
@@ -56,6 +89,13 @@ class Vision:
         return response
 
     def logo_detection(self, image) -> dict:
+        """
+        Detects logos in an image.
+        Args:
+            image (str): The image to be processed.
+        Returns:
+            dict: The response from the API.
+        """
         body = {"image": image, "operation": "logo_detection"}
         response = self.client.send_request(
             self.endpoint + "vision-ai/", form_data=body, method="post"
@@ -70,6 +110,13 @@ class Vision:
         return response
 
     def object_detection(self, image) -> dict:
+        """
+        Detects objects in an image.
+        Args:
+            image (str): The image to be processed.
+        Returns:
+            dict: The response from the API.
+        """
         body = {"image": image, "operation": "object_detection"}
         response = self.client.send_request(
             self.endpoint + "vision-ai/", form_data=body, method="post"
@@ -77,6 +124,14 @@ class Vision:
         return response
 
     def image_generation(self, prompt, output_size) -> dict:
+        """
+        Generates an image from a prompt.
+        Args:
+            prompt (str): The prompt for generating the image.
+            output_size (str): The size of the output image. e.g. 256x256, 512x512, 1024x1024, etc.
+        Returns:
+            dict: The response from the API.
+        """
         body = {"prompt": prompt, "output_size": output_size}
         response = self.client.send_request(
             self.endpoint + "image-generation/", form_data=body, method="post"
@@ -141,7 +196,15 @@ class NaturalLanguage:
         )
         return response
 
-    def text_to_speech(self, text, language_code) -> dict:
+    def text_to_speech(self, text: str, language_code: str) -> dict:
+        """
+        Generates audio from text.
+        Args:
+            text (str): The text to be converted to speech.
+            language_code (str): The language code of the text. e.g. en-US, en-GB, es-ES, etc.
+        Returns:
+            dict: The response from the API.
+        """
         body = {"text": text, "language_code": language_code}
         response = self.client.send_request(
             self.endpoint + "text-to-speech/", json_data=body, method="post"
@@ -149,6 +212,14 @@ class NaturalLanguage:
         return response
 
     def speech_to_text(self, audio, language_code) -> dict:
+        """
+        Converts audio to text.
+        Args:
+            audio (str): The audio to be converted to text.
+            language_code (str): The language code of the audio. e.g. en-US, en-GB, es-ES, etc.
+        Returns:
+            dict: The response from the API.
+        """
         body = {"audio": audio, "language_code": language_code}
         response = self.client.send_request(
             self.endpoint + "speech-to-text/", form_data=body, method="post"
@@ -156,6 +227,15 @@ class NaturalLanguage:
         return response
 
     def text_summary(self, text, max_length, min_length) -> dict:
+        """
+        Summarizes text.
+        Args:
+            text (str): The text to be summarized.
+            max_length (int): The maximum length of the summary.
+            min_length (int): The minimum length of the summary.
+        Returns:
+            dict: The response from the API.
+        """
         body = {"text": text, "max_length": max_length, "min_length": min_length}
         response = self.client.send_request(
             self.endpoint + "text-summary/", json_data=body, method="post"
@@ -180,6 +260,64 @@ class NaturalLanguage:
         body = {"text": text}
         response = self.client.send_request(
             self.endpoint + "text-classification/", json_data=body, method="post"
+        )
+        return response
+    
+    def essay_marker(self, question: str, essay: str, answer: str=None, word_length: int=None) -> dict:
+        """
+        This function is used to mark an essay based on a question and an answer.
+        Args:
+            question (str): The question to be answered.
+            essay (str): The essay to be marked.
+            answer (str): The answer to the question. If not provided, the question will be used to mark the essay.
+            word_length (int): The word length of the essay. If not provided, the final score will be returned without word length consideration.
+        Returns:
+            dict: The response from the API.
+        """
+        body = {"question": question, "essay": essay, "answer": answer, "word_length": word_length}
+        response = self.client.send_request(
+            self.endpoint + "essay-marker/", json_data=body, method="post"
+        )
+        return response
+    
+    def job_description_analyzer(self, job_description: str, resume_url: str) -> dict:
+        """
+        This function is used to analyze a job description and a resume to check for compatibility.
+        Args:
+            job_description (str): The job description to be analyzed.
+            resume_url (str): The url of the resume to be analyzed.
+        Returns:
+            dict: The response from the API.
+        """
+        body = {"job_description": job_description, "resume_url": resume_url}
+        response = self.client.send_request(
+            self.endpoint + "rank-resume/", json_data=body, method="post"
+        )
+        return response
+    
+    def employee_analyzer(self, survey: list) -> dict:
+        """
+        This function is used to analyze employee experience using survey data.
+        Args:
+            survey (List): The survey data to be analyzed. This is a list of dictionaries with the following structure:
+            [
+                {
+                    "question": "", // survey question
+                    "answer": "" // survey answer
+                },
+                {
+                    "question": "", // survey question
+                    "answer": "" // survey answer
+                }
+            ]
+        Returns:
+            dict: The response from the API.
+        """
+        body = {
+            "question_answers": survey
+        }
+        response = self.client.send_request(
+            self.endpoint + "employee-analysis/", json_data=body, method="post"
         )
         return response
 
@@ -222,90 +360,4 @@ class Voice:
         response = self.client.send_request(
             self.endpoint + "voice-cloning/tts/", form_data=body, method="post"
         )
-        return response
-
-
-class EssayMarker:
-    """Handles operations related to Autogon Qore."""
-
-    endpoint = "services/"
-
-    def __init__(self, client: any):
-        """Initializes the Production class.
-
-        Args:
-            client (any): The client object for making requests.
-        """
-        self.client = client
-
-    def essay_marker(self, question: str, essay: str, answer: str=None, word_length: int=None) -> dict:
-        """
-        This function is used to mark an essay based on a question and an answer.
-        Args:
-            question (str): The question to be answered.
-            essay (str): The essay to be marked.
-            answer (str): The answer to the question. If not provided, the question will be used to mark the essay.
-            word_length (int): The word length of the essay. If not provided, the final score will be returned without word length consideration.
-        Returns:
-            dict: The response from the API.
-        """
-        body = {"question": question, "essay": essay, "answer": answer, "word_length": word_length}
-        response = self.client.send_request(
-            self.endpoint + "essay-marker/", json_data=body, method="post"
-        )
-        return response
-    
-
-class HR_AI:
-    """Handles operations related to Autogon Qore."""
-    
-    endpoint = "services/"
-
-    def __init__(self, client: any):
-        """Initializes the Production class.
-
-        Args:
-            client (any): The client object for making requests.
-        """
-        self.client = client
-
-    def job_description_analyzer(self, job_description: str, resume_url: str) -> dict:
-        """
-        This function is used to analyze a job description and a resume to check for compatibility.
-        Args:
-            job_description (str): The job description to be analyzed.
-            resume_url (str): The url of the resume to be analyzed.
-        Returns:
-            dict: The response from the API.
-        """
-        body = {"job_description": job_description, "resume_url": resume_url}
-        response = self.client.send_request(
-            self.endpoint + "rank-resume/", json_data=body, method="post"
-        )
-        return response
-    
-    def employee_analyzer(self, survey: list) -> dict:
-        """
-        This function is used to analyze employee experience using survey data.
-        Args:
-            survey (List): The survey data to be analyzed. This is a list of dictionaries with the following structure:
-            [
-                {
-                    "question": "", // survey question
-                    "answer": "" // survey answer
-                },
-                {
-                    "question": "", // survey question
-                    "answer": "" // survey answer
-                }
-            ]
-        Returns:
-            dict: The response from the API.
-        """
-        body = {
-            "question_answers": survey
-        }
-        response = self.client.send_request(
-            self.endpoint + "employee-analysis/", json_data=body, method="post"
-        )
-        return response
+        return response     
