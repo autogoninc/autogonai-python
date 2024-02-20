f9e1cdc282dedeb8296f5238b0b2d5c24229a6ef
Maxiron <naarons800@gmail.com>
2024-02-20 17:33:58 +0100

    Update version to 0.1.3 in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding
 
 setup(
     name="autogonai-python",
-    version="0.1.2",
+    version="0.1.3",
     description="Python connector for Autogon Public APIs",
     author="Autogon AI Inc.",
     author_email="infrastructure@autogon.ai",
